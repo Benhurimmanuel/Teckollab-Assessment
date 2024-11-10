0d7984f8ad35061c82858835ad49d4e25e22a7ca
benhurimmanuel <benhurimmanuel@gmail.com>
2024-11-10 14:06:25 +0530

    changing text-case for linux deployment

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './LAYOUT';
 import { CssBaseline, Box } from '@mui/material';
 import { ThemeProviderWrapper } from './CONTEXT';
-import Home from "./PAGES/HOME"
+import Home from './PAGESssss/Home';
 
 function App() {
   return (
